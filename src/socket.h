af22f341e272f126f3e63209e4a64485bf5c53de
Jorge Sandez <sandez.jorge@gmail.com>
2019-08-29 00:52:06 -0300

    add methods to header

---
+++ src/socket.h
@@ -0,0 +1,16 @@
+#ifndef _SOCKET_H_
+#define _SOCKET_H_
+
+typedef struct socket {
+  int fd;
+} socket_t;
+
+int socket_send(socket_t *self, const void *buf, const size_t size);
+int socket_receive();
+void socket_init(socket_t *socket);
+void socket_release(socket_t *socket);
+void socket_connect(socket_t *socket, const char *host, const char *service);
+int socket_bind(socket_t *socket);
+int socket_listen(socket_t *socket, const char *service);
+
+#endif
